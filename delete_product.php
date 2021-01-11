ae896233b099e8050a8a777f184ac558bbd5fefb
hoangbgnh123456 <72736756+hoangbgnh123456@users.noreply.github.com>
2021-01-11 21:42:04 +0700

    Delete delete_product.php

--- delete_product.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-		include "connect.php";
-		$sql = "delete from song where songID = '$_GET[songID]'";
-		mysqli_query($con,$sql);
-		header('location:index_product.php?quanly=search_product');
-?>
